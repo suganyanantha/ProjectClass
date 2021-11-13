5d9fd54765551153da5382fa45e724ae4f95737b
suganyanantha <nk.suganya90@gmail.com>
2021-11-13 14:01:26 +0530

    first time start pushing

---
+++ src/test/java/com/projetclass/GITClass.java
@@ -0,0 +1,18 @@
+package com.projetclass;
+
+public class GITClass {
+	
+	public void method1() {
+		// TODO Auto-generated method stub
+
+	}
+	public void method2() {
+		// TODO Auto-generated method stub
+
+	}
+	public void method3() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
